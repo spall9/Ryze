01b80ebc16ad6b3348b2e95f538f6d058d0b026a
mar173 <kiker173@wp.pl>
2016-11-05 15:40:48 +0100

    Create AssemblyInfo.cs

---
+++ Properties/AssemblyInfo.cs
@@ -0,0 +1,17 @@
+#region Using directives
+using System;
+using System.Reflection;
+using System.Runtime.InteropServices;
+
+#endregion
+[assembly: AssemblyTitle ("PerfectCombo_Ryze")]
+[assembly: AssemblyDescription ("")]
+[assembly: AssemblyConfiguration ("")]
+[assembly: AssemblyCompany ("")]
+[assembly: AssemblyProduct ("PerfectCombo_Ryze")]
+[assembly: AssemblyCopyright ("Copyright 2016")]
+[assembly: AssemblyTrademark ("")]
+[assembly: AssemblyCulture ("")]
+[assembly: ComVisible (false)]
+[assembly: AssemblyVersion ("2016.11.05")]
+[assembly: Guid ("e95245f1-9e6a-4913-b2ed-639361e2595a")]
